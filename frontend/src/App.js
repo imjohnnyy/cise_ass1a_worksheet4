9d639ed580f5c134ed3be57b2373e649becd4d28
imjohnnyy <ldong4056@gmail.com>
2022-08-16 16:59:15 +1200

    fixed react routing issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,38 @@
 import React from "react";
-import {
-  Route,
-  NavLink,
-  BrowserRouter as Router
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, NavLink, Navigate} from "react-router-dom";
+import "./index.css"
 
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
-import SubmitArticle from "./pages/Submit-Article"; 
-//import NotFoundPage from "./pages/404";
+import SubmitArticle from "./pages/Submit-Article";
 
-const App = () =>  {
-    return (
-        <Router>
-        <div>
-          <h1>Software Engineering Empirical Evidence Database (SEED)</h1>
-            <ul className="header">
-                <li><NavLink exact to = "/">Home</NavLink></li>
-                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
-                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
-            </ul>
-          <div className="content">
-            <Route exact path="/" component={Home}/>
-            <Route  path="/SEPractice" component={SEPractice}/>
-            <Route  path="/SubmitArticle" component={SubmitArticle}/>
-          </div>
+
+const App = () => {
+  return (
+    <Router >
+      <div>
+        <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
+        <ul className="header">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/SEPractice" >Select the Practice</NavLink>
+          </li>
+          <li>
+            <NavLink to="/SubmitArticle" >Submit an Article</NavLink>
+          </li>
+        </ul>
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/SEPractice" element={<SEPractice />} />
+            <Route path="/SubmitArticle" element={<SubmitArticle />} />
+          </Routes>
         </div>
-        </Router>
-    );
-}
- 
+      </div>
+    </Router>
+  );
+};
+
 export default App;
