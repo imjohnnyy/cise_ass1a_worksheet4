a746f1230258da545531aeb3f92839e5d465b663
imjohnnyy <ldong4056@gmail.com>
2022-08-16 17:06:53 +1200

    included styling and 404 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, NavLink, Navigate} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, NavLink, Navigate } from "react-router-dom";
 import "./index.css"
 
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article";
-
+import NotFoundPage from "./pages/404";
 
 const App = () => {
   return (
@@ -14,7 +14,7 @@ const App = () => {
         <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
         <ul className="header">
           <li>
-            <NavLink to="/">Home</NavLink>
+            <NavLink exact to="/">Home</NavLink>
           </li>
           <li>
             <NavLink to="/SEPractice" >Select the Practice</NavLink>
@@ -28,6 +28,9 @@ const App = () => {
             <Route exact path="/" element={<Home />} />
             <Route path="/SEPractice" element={<SEPractice />} />
             <Route path="/SubmitArticle" element={<SubmitArticle />} />
+            {/* All other paths should redirect to NotFoundPage */}
+            <Route exact path="404" element={<NotFoundPage />} />
+            <Route path="/*" element={<Navigate to="404" replace />} />
           </Routes>
         </div>
       </div>
